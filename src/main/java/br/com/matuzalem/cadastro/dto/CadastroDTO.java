dd0d7ac3b3ee20823603db96f3c1aecd81c78898
matuzalembatista <matuzalem_pereira@hotmail.com>
2023-09-22 13:29:04 -0300

    Criando controlador de exclusão  de dados da lista

--- src/main/java/br/com/matuzalem/cadastro/dto/CadastroDTO.java
+++ src/main/java/br/com/matuzalem/cadastro/dto/CadastroDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class CadastroDTO {
-    
+    private Long id;
     private String email;
     private String name;
 }
--- src/main/java/br/com/matuzalem/cadastro/resources/CadastroResource.java
+++ src/main/java/br/com/matuzalem/cadastro/resources/CadastroResource.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.matuzalem.cadastro.dto.CadastroDTO;
 
@@ -15,16 +17,28 @@ public class CadastroResource {
 
     private List<CadastroDTO> cadastros = new ArrayList<>();
 
+    // COLETAR OS DADOS
     @PostMapping("cadastroPost")
     public String doPost(CadastroDTO dto, Model model) {
         cadastros.add(dto);
         return doGet(model);
-        
     }
 
+    // SALVAR OS DADOS EM LISTA
     @RequestMapping("cadastroGet")
-    public String doGet( Model model) {
+    public String doGet(Model model) {
         model.addAttribute("cadastros", cadastros);
         return "lista";
     }
+
+    // REMOVER O ITEM DA LISTA
+    @GetMapping("/cadastroDelete")
+    public String delete(@RequestParam("name") String name) {
+        cadastros.removeIf(cadastro -> name.equals(cadastro.getName()));
+        return "redirect:/cadastroGet";
+    }
+
+
+
+
 }
