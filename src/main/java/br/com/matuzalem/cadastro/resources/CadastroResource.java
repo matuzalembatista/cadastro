4450dce13487f2c2bf8c2f26b851970fe35fa2df
matuzalembatista <matuzalem_pereira@hotmail.com>
2023-09-22 18:47:24 -0300

    Criando controlador de edit de dados da lista

--- src/main/java/br/com/matuzalem/cadastro/resources/CadastroResource.java
+++ src/main/java/br/com/matuzalem/cadastro/resources/CadastroResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.matuzalem.cadastro.dto.CadastroDTO;
 
@@ -38,7 +39,39 @@ public class CadastroResource {
         return "redirect:/cadastroGet";
     }
 
+    // REENVIAR O ITEM EDITADO
+    @PostMapping("cadastroUpdate")
+    public String update(CadastroDTO updatedCadastro) {
+        // Encontre o cadastro original na lista e atualize os dados com base no email
+        for (int i = 0; i < cadastros.size(); i++) {
+            CadastroDTO cadastro = cadastros.get(i);
+            if (updatedCadastro.getName().equals(cadastro.getName())) {
+                cadastros.set(i, updatedCadastro);
+                break; // Parar após encontrar o cadastro
+            }
+        }
+        return "redirect:/cadastroGet";
+    }
 
+    // EDITAR O ITEM DA LISTA
+    @GetMapping("/cadastroPost")
+    public ModelAndView edit(@RequestParam("email") String email) {
+        ModelAndView mv = new ModelAndView("edit");
 
+        // Encontre o cadastro com o email fornecido
+        CadastroDTO cadastroToEdit = cadastros.stream()
+                .filter(cadastro -> email.equals(cadastro.getEmail()))
+                .findFirst()
+                .orElse(null);
+
+        if (cadastroToEdit != null) {
+            mv.addObject("cadastro", cadastroToEdit);
+        } else {
+            // Lidar com o caso em que o email não corresponde a nenhum cadastro
+            // Você pode redirecionar para uma página de erro ou tratar de outra forma
+        }
+
+        return mv;
+    }
 
 }
